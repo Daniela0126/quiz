b6fe3c29f6eb1b7c7e0008c25febe0d04da3c7c9
Daniela Valencia <daniela.valenica@gmail.com>
2019-01-07 22:59:26 +0000

    Done

--- js/script.js
+++ js/script.js
@@ -10,23 +10,42 @@ $(document).ready(function() {
         var q3Result = $("#question3").val();
         var q4Result = $("#question4").val();
         var q5Result = $("#question5").val();
+        var totalScore = living(q1Result) + rather(q2Result) + personality(q3Result) + food(q4Result) + sociality(q5Result);
 
-        var totalScore = living(q1Result) + rather(q2Result) + personality(q3Result) + sociality(q5Result) + food(q4Result);
+var finalAnswer = final(totalScore, name)
+alert(finalAnswer)
     });
 
-});
+
+ function final(totalScore, name){
+    if(totalScore > 30 && totalScore < 40){
+        return name + ", you're a Wolf"
+    }
+    
+    else if(totalScore > 40 && totalScore < 50){
+        return name + ", you're a Lion"
+    }
+    
+    else if(totalScore > 20 && totalScore < 30){
+        return name + ", you're a Tiger"
+    }
+    else if(totalScore < 20){
+        return name + ", you're a Shark"
+    }
+}
+
 
 function living(name){
-    if(name === "Savannah" || "savannah"){
+    if(name === "Savannah" || name === "savannah"){
         return 10 ;
     }
-    else if(name === "Coast Line" || "coast line"){
+    else if(name === "Coast Line" || name === "coast line"){
         return 7 ;
     }
-    else if(name === "Forest" || "forest"){
+    else if(name === "Forest" || name === "forest"){
         return 4;
     }
-    else if(name === "Jungle" || "jungle"){
+    else if(name === "Jungle" || name === "jungle"){
         return 4 ;
     }
     else{
@@ -35,46 +54,53 @@ function living(name){
     
 }
 
+
 function rather(action){
-    if(action === "lay around" || "Lay around" || "Lay Around"){
+    if(action === "lay around" || action === "Lay around" || action ===  "Lay Around"){
         return 10;
     }
-    else if(action === "swim" || "Swim"){
+    else if(action === "swim" || action ===  "Swim"){
         return 7 ;
     }
-    else if(action === "Play" || "play"){
+    else if(action === "Play" || action ===  "play"){
         return 5;
     }
-    else if(action === "excercise" || "excercise"){
+    else if(action === "Excercise" || action ===  "excercise"){
         return 5 ;
+    }
+    else{
+        return 0;
     }
 }
 
 
 function personality(pick){
-    if(pick === "loyal" || "loyal"){
+    if(pick === "Loyal" || pick === "loyal"){
         return 5
     }
-    else if(pick === "loner" || "Loner"){
+    else if(pick === "loner" || pick === "Loner"){
         return 5 
     }
-    else if(pick === "both" || "Both"){
+    else if(pick === "both" || pick === "Both"){
         return 10 
+    }
+    else{
+        return 0;
     }
     
 }
 
 function sociality(chose){
-    if(chose === "Friendly" || "friendly"){
+    if(chose === "Friendly" || chose === "friendly"){
         return 5
     }
-    else if(chose === "Anti-social" || "antisocial" || "anti-social"){
+    else if(chose === "Anti-social" || chose === "antisocial" || chose === "anti-social"){
         return 5
     }
-    else if(chose === "Very social" || "very social" || "social"){
+    else if(chose === "Very social" || chose === "very social" || chose === "social"){
         return 10
     }
-    else if(chose === "All of the Above" || "all of the above"){
+    else if(chose === "All of the Above" || chose === "all of the above"){
         return 13
     }
     else{
@@ -83,16 +109,18 @@ function sociality(chose){
 }
 
 function food(preference){
-    if(preference === "Chicken" || "chicken"){
+    if(preference === "Chicken" || preference === "chicken"){
         return 5
     }
-    else if(preference === "Fish" || "fish"){
+    else if(preference === "Fish" || preference === "fish"){
         return 5
     }
-    else if(preference === "Meat" || "meat"){
+    else if(preference === "Meat" || preference === "meat"){
         return 5
     }
     else{
         return 0
     }
 }
+
+});
